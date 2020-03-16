1c72769a74365e3ba5bea662177be498151924e8
roseeDuMatin <s_pelluet@hotmail.fr>
2020-03-16 16:04:08 +0100

    v1.1

---
+++ README.md
@@ -0,0 +1,5 @@
+# Partie 1
+## Exercice 1
+Lance un serveur qui écoute au port 3000 et retourne le texte "Hello World !"
+
+
---
+++ server.js
@@ -0,0 +1,15 @@
+//console.log("Hello World")
+// test
+
+const express = require('express') ;
+const app = express() ;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!') ;
+})  
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + ' !');
+})
