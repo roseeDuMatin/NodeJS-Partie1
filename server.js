13fca87531054bfd60e40a2a8ac7d7077aaacf7d
roseeDuMatin <s_pelluet@hotmail.fr>
2020-03-16 16:11:48 +0100

    Route /hello

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-//console.log("Hello World")
-// test
-
 const express = require('express') ;
 const app = express() ;
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!') ;
-})  
+});  
 
-var port = process.env.PORT || 3000;
+app.get('/hello:name', function (req, res){
+  var name = req.params.name;
+
+  if(name != null && name != ''){
+    res.send('Hello ' + req.params.name + ' !');
+  }else{
+    res.send('Hey Stranger ;)');
+  }
+});
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + ' !');
-})
+});
